0746999bab9d9eb9ec2a50488caaa48fcb8ad0d0
Kengo TODA <skypencil@gmail.com>
2018-06-06 18:32:23 +0800

    initial commit

---
+++ src/test/java/jp/skypencil/ApplicationTest.java
@@ -0,0 +1,18 @@
+package jp.skypencil;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ApplicationTest {
+  @Test
+  public void test(ApplicationContext context) {
+    assertNotNull(context);
+  }
+}
