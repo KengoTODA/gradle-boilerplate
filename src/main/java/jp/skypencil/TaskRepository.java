9a53179f2d9c9344d3f9fd0834e3302fce36a055
Kengo TODA <skypencil@gmail.com>
2021-01-27 21:10:37 +0800

    feat: list existing tasks when the page is loaded

--- src/main/java/jp/skypencil/TaskRepository.java
+++ src/main/java/jp/skypencil/TaskRepository.java
@@ -1,14 +1,15 @@
 package jp.skypencil;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 import org.springframework.stereotype.Repository;
 
 @Repository
 class TaskRepository {
-  private final Map<TaskId, Task> store = new ConcurrentHashMap<>();
+  private final Map<TaskId, Task> store = Collections.synchronizedMap(new LinkedHashMap<>());
 
   void save(Task task) {
     store.put(task.getId(), task);
