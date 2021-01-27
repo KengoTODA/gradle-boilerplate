c5ce50134dce16165d6c783af85a085426162f1d
Kengo TODA <skypencil@gmail.com>
2021-01-27 20:13:50 +0800

    feat: add API to handle tasks

---
+++ src/main/java/jp/skypencil/TaskController.java
@@ -0,0 +1,42 @@
+package jp.skypencil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+
+/**
+ * The Controller handling feature regarding tasks.
+ * Just translate rest-controller related values to help domain objects understand requests from user.
+ */
+@RestController
+public class TaskController {
+    private final TaskDomainService service;
+
+    @Autowired
+    public TaskController(TaskDomainService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/task/{id}")
+    public TaskModel find(@PathVariable("id") UUID id) {
+        return service.find(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No task found with id: " + id));
+    }
+
+    @GetMapping("/task/")
+    public Stream<TaskModel> list() {
+        return service.list();
+    }
+
+    @PostMapping("/task")
+    public TaskModel create(@RequestBody String subject) {
+        return service.create(subject);
+    }
+}
---
+++ src/main/java/jp/skypencil/TaskId.java
@@ -0,0 +1,38 @@
+package jp.skypencil;
+
+import java.util.Objects;
+import java.util.UUID;
+
+class TaskId implements Comparable<TaskId> {
+    private final UUID uuid;
+
+    TaskId(UUID uuid) {
+        this.uuid = Objects.requireNonNull(uuid);
+    }
+
+    UUID getUuid() {
+        return uuid;
+    }
+
+    static TaskId create() {
+        return new TaskId(UUID.randomUUID());
+    }
+
+    @Override
+    public int compareTo(TaskId another) {
+        return this.uuid.compareTo(another.uuid);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskId taskId = (TaskId) o;
+        return Objects.equals(uuid, taskId.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+}
