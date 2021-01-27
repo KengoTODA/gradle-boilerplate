d441c9b2608705997e79acc71035ad74c2852171
Kengo TODA <skypencil@gmail.com>
2021-01-27 20:55:56 +0800

    feat: create a simple page to create and list tasks

--- src/main/java/jp/skypencil/Task.java
+++ src/main/java/jp/skypencil/Task.java
@@ -5,44 +5,45 @@ import java.util.Objects;
 /**
  * The mutable entity representing task to handle.
  *
- * We use only {@link #id} to identify the instance, because other fields could be updated by user requests.
+ * <p>We use only {@link #id} to identify the instance, because other fields could be updated by
+ * user requests.
  */
 class Task {
-    private final TaskId id;
-    private final String subject;
-    private final boolean done;
+  private final TaskId id;
+  private final String subject;
+  private final boolean done;
 
-    Task(TaskId id, String subject, boolean done) {
-        this.id = Objects.requireNonNull(id);
-        this.subject = Objects.requireNonNull(subject);
-        this.done = done;
-    }
+  Task(TaskId id, String subject, boolean done) {
+    this.id = Objects.requireNonNull(id);
+    this.subject = Objects.requireNonNull(subject);
+    this.done = done;
+  }
 
-    // TODO: confirm the usage of the getter
-    TaskId getId() {
-        return id;
-    }
+  // TODO: confirm the usage of the getter
+  TaskId getId() {
+    return id;
+  }
 
-    // TODO: confirm the usage of the getter
-    String getSubject() {
-        return subject;
-    }
+  // TODO: confirm the usage of the getter
+  String getSubject() {
+    return subject;
+  }
 
-    // TODO: confirm the usage of the getter
-    boolean isDone() {
-        return done;
-    }
+  // TODO: confirm the usage of the getter
+  boolean isDone() {
+    return done;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return Objects.equals(id, task.id);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Task task = (Task) o;
+    return Objects.equals(id, task.id);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
--- src/main/java/jp/skypencil/TaskRepository.java
+++ src/main/java/jp/skypencil/TaskRepository.java
@@ -1,25 +1,24 @@
 package jp.skypencil;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
+import org.springframework.stereotype.Repository;
 
 @Repository
 class TaskRepository {
-    private final Map<TaskId, Task> store = new ConcurrentHashMap<>();
+  private final Map<TaskId, Task> store = new ConcurrentHashMap<>();
 
-    void save(Task task) {
-        store.put(task.getId(), task);
-    }
+  void save(Task task) {
+    store.put(task.getId(), task);
+  }
 
-    Optional<Task> find(TaskId id) {
-        return Optional.ofNullable(store.get(id));
-    }
+  Optional<Task> find(TaskId id) {
+    return Optional.ofNullable(store.get(id));
+  }
 
-    Stream<Task> list() {
-        return store.values().stream();
-    }
+  Stream<Task> list() {
+    return store.values().stream();
+  }
 }
