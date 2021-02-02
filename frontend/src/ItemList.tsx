0cc3c1fbcacdac71cf75b524ed2ac052c2dd5f9f
Kengo TODA <skypencil@gmail.com>
2021-02-02 20:35:43 +0800

    style: apply prettier

--- frontend/src/ItemList.tsx
+++ frontend/src/ItemList.tsx
@@ -1,25 +1,29 @@
-import React,{Component} from 'react'
-import {TaskData} from './data'
-import { List, ListItem } from '@material-ui/core';
+import React, { Component } from "react";
+import { TaskData } from "./data";
+import { List, ListItem } from "@material-ui/core";
 
-class Item extends Component<{item:TaskData}> {
-    constructor(props: {item:TaskData}) {
-        super(props)
-    }
-    render() {
-        return (<ListItem>{this.props.item.subject} (id: {this.props.item.id})</ListItem>)
-    }
+class Item extends Component<{ item: TaskData }> {
+  constructor(props: { item: TaskData }) {
+    super(props);
+  }
+  render() {
+    return (
+      <ListItem>
+        {this.props.item.subject} (id: {this.props.item.id})
+      </ListItem>
+    );
+  }
 }
 
 export class ItemList extends React.Component<{ items: TaskData[] }> {
-    constructor(props: { items: TaskData[] }) {
-        super(props)
-    }
+  constructor(props: { items: TaskData[] }) {
+    super(props);
+  }
 
-    render() {
-        const items = this.props.items.map(item => (<Item key={item.id} item={item} />))
-        return (<List id="task">
-            {items}
-        </List>)
-    }
+  render() {
+    const items = this.props.items.map((item) => (
+      <Item key={item.id} item={item} />
+    ));
+    return <List id="task">{items}</List>;
+  }
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-      filename: 'index.html'
+      template: path.join(__dirname, "src/index.html"),
+      filename: "index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
