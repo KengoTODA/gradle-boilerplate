d1477ff3d925f6da940a70dfae4369fbb05e47c6
Kengo TODA <skypencil@gmail.com>
2021-02-02 20:50:32 +0800

    fix: Introduce React (#354)
    * build: rename the bundle JS by its content hash
    
    * fix: introduce React and ReactDOM
    
    * fix: introduce material-ui
    
    * style: apply prettier
    
    * build: reorganize build tools
    
    * build: reorganize type defs

---
+++ frontend/src/Form.tsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Button, TextField } from "@material-ui/core";
+import {
+  createStyles,
+  withStyles,
+  Theme,
+  WithStyles,
+} from "@material-ui/core/styles";
+
+const useStyles = (theme: Theme) =>
+  createStyles({
+    root: {
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  });
+
+interface Props extends WithStyles<typeof useStyles> {}
+
+class Form extends Component<
+  Props & { onSubmit: (subject: string) => void },
+  { subject: string }
+> {
+  constructor(props: Props & { onSubmit: (subject: string) => void }) {
+    super(props);
+    this.state = { subject: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.subject);
+    this.setState((state) => ({
+      subject: "",
+    }));
+  }
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState((state) => ({
+      subject: e.target.value,
+    }));
+  }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form
+        className={classes.root}
+        id="form"
+        action="/task"
+        method="post"
+        onSubmit={this.handleSubmit}
+      >
+        <TextField
+          id="subject"
+          label="Subject"
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.subject}
+          autoFocus
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    );
+  }
+}
+
+export default withStyles(useStyles)(Form);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.tsx",
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Task management app',
+      template: path.join(__dirname, "src/index.html"),
+      filename: "index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
